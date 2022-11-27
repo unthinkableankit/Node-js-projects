1d0b098c4cf1bbf788d6274500890cfb7957cb21
ankit <ankit@unthinkable.co>
2022-11-27 17:48:40 +0530

    templete engine folder updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const friendsRouter = require("./routes/friends.router");
 const messageRouter = require("./routes/message.router");
@@ -12,11 +13,21 @@ app.use((req, res, next) => {
   const charli = Date.now() - delta;
   console.log(`${req.method}${req.baseUrl} ${req.url} ${charli}`);
 });
-app.use('/site',express.static('public'))//express static file middleware
+app.use("/site", express.static("public")); //express static file middleware
 app.use(express.json()); // json parsing middleware
 
 app.use("/friends", friendsRouter);
 app.use("/messages", messageRouter);
+// template engine
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "let's learn Ubuntu",
+    caption: "Ubuntu is open sourse opereating system",
+  });
+});
 
 //page not found route
 app.get("/*", (req, res) => {
